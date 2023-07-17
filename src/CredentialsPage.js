42e3d10dccbad3689c0a7b157a7a42d177b15ba0
Surajit02rambo <82580421+Surajit02rambo@users.noreply.github.com>
2023-07-17 20:47:55 +0530

    Update CredentialsPage.js

--- src/CredentialsPage.js
+++ src/CredentialsPage.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
 
 const CredentialsPage = () => {
   const location = useLocation();
@@ -7,15 +10,27 @@ const CredentialsPage = () => {
   const username = searchParams.get('username');
   const password = searchParams.get('password');
   const email = searchParams.get('email');
+
+  useEffect(() => {
+    // Redirect to LoginForm page when the component mounts
+    const timer = setTimeout(() => {
+      history.push('/');
+    }, 3000); // Redirect after 3 seconds
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div className='dashboard'>
       <div className='check'></div>
       <h2>Logged In Successfully!</h2>
       <pre>Welcome To Dashboard</pre>
       <div className='credentials'>
-      <p>Username: {username}</p>
-      <p>Password: {password}</p>
-      <p>Email: {email}</p>
+        <p>Username: {username}</p>
+        <p>Password: {password}</p>
+        <p>Email: {email}</p>
       </div>
     </div>
   );
